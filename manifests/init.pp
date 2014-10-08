5abf670789da1937353e86901fdbb4f564f801bf
Matteo Alessani <alessani@gmail.com>
2014-10-08 17:41:06 +0200

    removed db

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,10 @@ class install_mysql_server (
   $root_password = 'root',
   $innodb_buffer_pool_size = '128M',
   $application_user = 'user',
-  $application_password = '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19' # password,
+  $application_password = '*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19', # password
   $backup_password = 'backup',
   $backup_dir = '/var/backups',
   $data_dir = '/var/lib/mysql',
-  $databases = []
 ) {
 
   class {'::mysql::server':
@@ -14,16 +13,6 @@ class install_mysql_server (
     data_dir => $data_dir,
     override_options => {
       'mysqld' => {'innodb_buffer_pool_size' => $innodb_buffer_pool_size}
-    },
-    databases => { $databases:
-      ensure => present,
-      charset => 'utf8'
-    },
-    users => {
-      "$application_user@localhost" => {
-        ensure => present,
-        password_hash => $application_password
-      }
     }
   }
 
